35c59896dbe8a65b1e48466519b10e3102fea01e
Zack Jones <zackaryj@rams.colostate.edu>
2021-06-03 10:43:39 -0600

    Add franz's youtube channel videos + Order all youtube videos

--- api.php
+++ api.php
@@ -1,28 +1,29 @@
 <?php
 
-function kcw_movies_api_search(WP_REST_Request $request ) {
-    echo "HERE";
+function kcw_movies_api_search($data) {
+    return "YEP";
 }
-function kcw_movies_api_page(WP_REST_Request $request ) {
-    echo "HERE";
+
+function kcw_movies_api_page($data) {
+    return "YEP";
 }
 
 add_action('rest_api_init', "kcw_movies_init_api");
 function kcw_movies_init_api() {
     register_rest_route('kcwmovies/v1', 
-                        '/search/?P<search\s+>', 
+                        '/search/(?P<msearch>[\s+])', 
                         array(
                             'methods'=> 'GET',
                             'callback'=> 'kcw_movies_api_search'
                         )
     );
     register_rest_route('kcwmovies/v1', 
-                        '/page/?P<page\d+>', 
+                        '/page/(?P<lpage>\d+)', 
                         array(
                             'methods'=> 'GET',
                             'callback'=> 'kcw_movies_api_page'
                         )
     );
 }
-
+add_action( 'rest_api_init', "kcw_movies_init_api");
 ?>
